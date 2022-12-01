667bfd255c445949100634146d825c8f2dacf38d
Thomashallberg <thomas.hallberg91@outlook.com>
2022-12-01 17:46:27 +0100

    Lade till bokningar

--- Main.py
+++ Main.py
@@ -11,34 +11,52 @@ class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_size = db.Column(db.Integer, unique=False, nullable=False)
     bed_count = db.Column(db.Integer, unique=False, nullable=False)
+    bokningar = db.relationship('Booking', backref='room', lazy=True)
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     namn = db.Column(db.String(80), unique=False, nullable=False)
     telefonnummer = db.Column(db.String(80), unique=False, nullable=False)
+    bokningar = db.relationship('Booking', backref='customer', lazy=True)
+    
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    start_date = db.Column(db.Date, unique=False, nullable=False)
+    end_date = db.Column(db.Date, unique=False, nullable=False)
     
 if __name__  == "__main__":
     with app.app_context():
         upgrade()
 
 
-with app.app_context():
-    while True:
-        print("1. Register customer")
-        print("2. Edit customer")
-        sel = input("What would you like to do?:")
+    with app.app_context():
+        while True:
+            print("1. Register customer")
+            print("2. Edit customer")
+            print("3. Book a room")
+            sel = input("What would you like to do?:")
 
-        if sel == "1":
-            c = Customer()
-            c.namn = input("Ange namn:")
-            c.telefonnummer = input("Ange telefonnummer:")
-            db.session.add(c)
-            db.session.commit()
-        if sel == "2":
-            for x in Customer.query.all():
-                print(f"{x.id} {x.namn} {x.telefonnummer}")
-            sel = int(input("Vilket kund id vill du ändra?"))
-            u = Customer.query.filter_by(id=sel).first()
-            u.namn = input("Ange nytt namn:")
-            u.telefonnummer = input("Ange nytt nummer:")
-            db.session.commit()
+            if sel == "1":
+                c = Customer()
+                c.namn = input("Ange namn:")
+                c.telefonnummer = input("Ange telefonnummer:")
+                db.session.add(c)
+                db.session.commit()
+            if sel == "2":
+                for x in Customer.query.all():
+                    print(f"{x.id} {x.namn} {x.telefonnummer}")
+                sel = int(input("Vilket kund id vill du ändra?"))
+                u = Customer.query.filter_by(id=sel).first()
+                u.namn = input("Ange nytt namn:")
+                u.telefonnummer = input("Ange nytt nummer:")
+                db.session.commit()
+            if sel == "3":
+                b = Booking()
+                b.customer_id = input("Ange customer ID:")
+                b.room_id = input("Ange Room ID")
+                b.start_date = input("Datum format (yyyy-mm-dd")
+                b.end_date = input("Datum format (yyyy-mm-dd")
+                db.session.add(b)
+                db.session.commit()
